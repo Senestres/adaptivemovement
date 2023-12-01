117d39d38ada3216c8f8ef0dcbcc52986719d795
Senestres <100228689+Senestres@users.noreply.github.com>
2023-12-01 10:45:18 +0100

    sitemap

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Adaptive Movement",
-	url: "https://senestres.github.io/adaptive-movement/",
+	url: "https://adaptivemovement.net/",
 	language: "en",
 	description: "Skill acquisition and movement education",
 	author: {
 		name: "Yann Daout",
 		//email: "youremailaddress@example.com",
-		url: "https://senestres.github.io/adaptive-movement/about/"
+		url: "https://adaptivemovement.net/about/"
 	}
 }
